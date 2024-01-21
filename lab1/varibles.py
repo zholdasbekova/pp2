6c4e4dbb57d6d742c157242be44be4c792c75a24
zholdasbekova <156904067+zholdasbekova@users.noreply.github.com>
2024-01-21 23:00:37 +0600

    prog

---
+++ lab1/varibles.py
@@ -0,0 +1,27 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+a = 4
+A = "Sally"
+#A will not overwrite a
